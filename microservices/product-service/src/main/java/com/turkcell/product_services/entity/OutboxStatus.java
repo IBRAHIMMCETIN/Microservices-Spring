e01e07423630f3bb74a5efee28ef0c57f8a2edce
IBRAHIMMCETIN <ibrahimcetin776@gmail.com>
2026-06-08 15:51:06 +0300

    feignclients

---
+++ microservices/product-service/src/main/java/com/turkcell/product_services/entity/OutboxStatus.java
@@ -0,0 +1,3 @@
+package com.turkcell.product_services.entity;
+
+public enum OutboxStatus { PENDING, SENT, FAILED }
---
+++ microservices/product-service/src/main/java/com/turkcell/product_services/entity/TestClass.java
@@ -0,0 +1,5 @@
+package com.turkcell.product_services.entity;
+
+public record TestClass(String message) {
+
+}
